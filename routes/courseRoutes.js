8a1d80827fe199a8d69f8e402ca4a7d45cbd804a
Ronny <noudjemambe@gmail.com>
2025-04-09 01:31:24 +0200

    entering in student functionality

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Course = require('../models/course');
-
 // Manage Courses
 router.get('/manage', async (req, res) => {
     const courses = await Course.find();
